25d457c7430d88167d62b053a5d072d588c9c95b
ming.z <mithvv@gmail.com>
2015-07-09 18:03:24 +0800

    新增通讯录查询功能 v0.1

--- app.js
+++ app.js
@@ -2,10 +2,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var todos = require('./routes/todos');
+var contact_bot = require('./routes/contact_bot');
 var cloud = require('./cloud');
+var xml2js = require('xml2js');
 
 var app = express();
+
+
+function xmlBodyParser(req, res, next) {
+    // if (req.body) return next();
+    // req.body = req.body || {};
+ 
+    // ignore GET
+    // if ('GET' == req.method || 'HEAD' == req.method) return next();
+ 
+    // // check Content-Type
+    if ('text/xml' != req.get('Content-Type')) return next();
+ 
+    // flag as parsed
+    req._body = true;
+ 
+    // parse
+    var buf = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk){ buf += chunk });
+    req.on('end', function(){  
+      var parseString = xml2js.parseString;
+      parseString(buf, function(err, json) {
+        if (err) {
+            err.status = 400;
+            next(err);
+        } else {
+            req.body = json;
+            next();
+        }
+      });
+    });
+};
+
+
 
 // 设置 view 引擎
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +52,7 @@ app.use(cloud);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(xmlBodyParser);
 app.use(cookieParser());
 
 app.get('/', function(req, res) {
@@ -24,7 +60,7 @@ app.get('/', function(req, res) {
 })
 
 // 可以将一类的路由单独保存在一个文件中
-app.use('/todos', todos);
+app.use('/contact_bot', contact_bot);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
---
+++ routes/contact_bot.js
@@ -0,0 +1,53 @@
+var router = require('express').Router();
+var AV = require('leanengine');
+var xml2js = require('xml2js');
+var xml = require('xml');
+
+
+// `AV.Object.extend` 方法一定要放在全局变量，否则会造成堆栈溢出。
+var Contents = AV.Object.extend('Contents');
+
+router.get('/', function(req, res, next) {
+  res.send(req.query.echostr);
+});
+
+router.post('/', function(req, res, next) {
+
+  var nameText = req.body.xml.Content[0];
+  var toUserName = req.body.xml.FromUserName[0];
+
+  var query = new AV.Query(Contents);
+  query.equalTo("cnname", nameText);
+  query.first({
+    success: function(obj) {
+
+      var msgText = "找不到你呀，输错了吧~";
+      if (obj) {
+        msgText = "我抓住你了：" + obj.get("enname") + 
+        "\n部门:" + obj.get("sector") +
+        // "\n邮箱:" + obj.get("mail") +
+        // "\n手机号:" + obj.get("mobile") +
+        "\nQQ:" + obj.get("qq") + "\n安全考虑，暂时只显示 QQ 哦";
+      }
+
+
+      var xmlStr = xml( [{
+        "xml" : [
+          { "ToUserName" : { _cdata: toUserName } },
+          { "FromUserName" : { _cdata: "gh_d832680c1932" } },
+          { CreateTime : 12312341 },
+          { MsgType : { _cdata: "text" } },
+          { "Content" : { _cdata: msgText } }
+        ]
+      }]);
+
+      res.send(xmlStr);
+    },
+    error: function(err) {
+      res.send("err" + err);
+    }
+  });
+
+})
+
+module.exports = router;
